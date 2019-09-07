133089c0c4d6c738ec39bb84c65bd47a35cbcb23
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-09-07 15:10:31 +0200

    Delete contact form

--- src/views/ContactView/ContactView.js
+++ src/views/ContactView/ContactView.js
@@ -8,16 +8,8 @@ const ContactView = () => (
         <Logo />
         <h2 className={styles.title}>Jarek Miś</h2>
         <p>Napisz do mnie:</p>
-        <p>J.mis@outlook.com</p>
-        <form action="https://formspree.io/kamil.gaszowski@gmail.com" method="POST">
-            <label>Imię:</label>
-            <input type="text" name="name" />
-            <label>Twój imejl:</label>
-            <input type="email" name="_replyto" />
-            <label>Twoja wiadomość:</label>
-            <textarea type='text' name='message'/>
-            <input type="submit" value="Send" />
-        </form>
+        <p mailto='J.mis@outlook.com'>J.mis@outlook.com</p>
+
     </div>
 );
 
