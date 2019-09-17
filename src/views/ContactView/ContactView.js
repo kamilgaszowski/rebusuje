faaf979f297a7d0a0a3f9520260972d73b869d6c
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-09-17 12:03:32 +0200

    Delete forms - still doesn't work

--- src/views/ContactView/ContactView.js
+++ src/views/ContactView/ContactView.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './ContactView.module.scss';
 import Logo from '../../components/Logo/Logo';
-import NetlifyForm from 'react-netlify-form'
 
 
 const ContactView = () => (
@@ -10,28 +9,7 @@ const ContactView = () => (
         <h2 className={styles.title}>Jarek Miś</h2>
         <p>Napisz do mnie:</p>
         <a href='mailto:J.mis@outlook.com'>J.mis@outlook.com</a>
-        <NetlifyForm name='Contact Form'>
-            {({ loading, error, success }) => (
-                <div>
-                    {loading &&
-                        <div>Loading...</div>
-                    }
-                    {error &&
-                        <div>Your information was not sent. Please try again later.</div>
-                    }
-                    {success &&
-                        <div>Thank you for contacting us!</div>
-                    }
-                    {!loading && !success &&
-                        <div>
-                            <input type='text' name='Name' placeholder='imię' required />
-                            <textarea name='Message' placeholder='wiadomość' required />
-                            <button>Submit</button>
-                        </div>
-                    }
-                </div>
-            )}
-        </NetlifyForm>
+
 
     </div>
 );
